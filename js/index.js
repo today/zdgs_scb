bfa891432879111cc6de3cae15fe210ec22314e8
Jin Tian <today.xa@gmail.com>
2016-03-15 16:47:10 +0800

    检查数据，并把所有的数据集中到一个数组里。

--- js/index.js
+++ js/index.js
@@ -5,26 +5,31 @@
  */
 var xlsx = require("node-xlsx");
 
-var ORDER_DETAIL = null;
+var ORDER_DETAIL = [];
 //var TAX_RATE = 1.17 ;
 
 var init_100 = function(){
   MSG.put("系统启动。");
 
   // 充当数据源的文件 的文件名 的关键字
-  vm.src_files_flag.push("到货数据");
-  vm.src_files_flag.push("自有调社会");
-  vm.src_files_flag.push("终端销售明细表咸阳");
-  vm.src_files_flag.push("终端销售明细表商洛");
-  vm.src_files_flag.push("终端销售明细表安康");
-  vm.src_files_flag.push("终端销售明细表宝鸡");
-  vm.src_files_flag.push("终端销售明细表延安");
-  vm.src_files_flag.push("终端销售明细表榆林");
-  vm.src_files_flag.push("终端销售明细表汉中");
-  vm.src_files_flag.push("终端销售明细表渭南");
-  vm.src_files_flag.push("终端销售明细表西安");
-  vm.src_files_flag.push("终端销售明细表铜川");
+  other_files = [];
+  other_files.push("到货数据");
+  other_files.push("自有调社会");
 
+  order_files = [];
+  //order_files.push("终端销售明细表咸阳");
+  //order_files.push("终端销售明细表商洛");
+  order_files.push("终端销售明细表安康");
+  //order_files.push("终端销售明细表宝鸡");
+  //order_files.push("终端销售明细表延安");
+  //order_files.push("终端销售明细表榆林");
+  //order_files.push("终端销售明细表汉中");
+  order_files.push("终端销售明细表渭南");
+  order_files.push("终端销售明细表西安");
+  //order_files.push("终端销售明细表铜川");
+
+  vm.src_files_flag = order_files.concat(other_files);
+  vm.order_files_flag = order_files;
 }
 
 var check_env_110 = function(){
@@ -50,6 +55,7 @@ var select_file_120 = function(){
   return true;
 };
 
+// 检查输入文件是否齐备。
 var check_src_130 = function(){
   var run_flag = true;
 
@@ -58,7 +64,7 @@ var check_src_130 = function(){
   vm.base_dir = path.dirname(temp_path ) + "/" ;
   console.log("base_dir: " + vm.base_dir );
 
-  // 程序运行所必须的数据源
+  // 自动寻找 程序运行所必须的数据源
   vm.src_files = find_src_file(vm.base_dir, vm.src_files_flag);
   
   console.log( vm.src_files);
@@ -76,13 +82,40 @@ var check_order_140 = function(){
   var run_flag = true;
   
   //  终端销售明细表  格式检查
-  var must_field3 = [];
-  must_field3.push("物料编码");
-  must_field3.push("机型");
-  must_field3.push("归属地州");
-  must_field3.push("手机串码");
+  var must_field = [];
+  must_field.push("物料编码");
+  must_field.push("机型");
+  must_field.push("归属地州");
+  must_field.push("手机串码");
+  ORDER_DETAIL.push(must_field);
 
   // 循环检查所有销售订单文件
+  for( var i=0; i<vm.order_files_flag.length; i++ ){
+    var temp_filename = vm.src_files[vm.order_files_flag[i]];
+    console.log(temp_filename);
+
+    var obj = null;
+    obj = xlsx.parse( vm.base_dir + temp_filename ); // 读入xlsx文件
+    // console.log(vm.src_files['销售订单明细']);
+    // console.log(obj);
+    
+    // 取出第一个sheet 的第一行。检查标题栏的内容是否正确
+    var order_info = obj[0].data;
+    var all_title = order_info[0];
+    console.log(all_title);
+    run_flag = check_must_title(all_title, must_field);
+
+    if( run_flag ){
+      var index_must = [];
+      index_must = find_title_index_from_array(all_title, must_field);
+      
+      console.log(index_must);
+      order_info2 = select_col_from_array( order_info, index_must);
+      ORDER_DETAIL = ORDER_DETAIL.concat( _.rest(order_info2) );
+      console.log("ORDER_DETAIL.length=" + ORDER_DETAIL.length);
+    }
+    
+  }
 
 
 
@@ -102,6 +135,10 @@ var check_arrival_150 = function(){
   must_field.push("移出物料");
   must_field.push("移出物料描述");
   must_field.push("移入库位描述");
+
+  console.log("ORDER_DETAIL.length=" + ORDER_DETAIL.length);
+
+  return true;
 }
 
 var check_transfer_160 = function(){
@@ -126,14 +163,23 @@ var save_to_db = function(){
 
 
 var check_must_title = function(target, keywords ){
+  var run_flag = true;
   for(var i=0; i<keywords.length; i++ ){
     var kw = keywords[i];
     if( -1 === _.indexOf(target, kw)){
       // 显示出错提示。
       ERR_MSG.put("数据出错：。无法找到「"+ kw +"」列，请检查数据的第一行。" );
+      run_flag = false;
     }
   }
+  return run_flag;
 }
+
+
+
+
+
+
 
 
 
