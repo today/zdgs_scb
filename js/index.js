e502105e44e2445e1c33ea11f443c3b0b13a63ef
Jin Tian <today.xa@gmail.com>
2016-03-17 23:47:26 +0800

    计算物料分项汇总  90%

--- js/index.js
+++ js/index.js
@@ -17,16 +17,16 @@ var init_100 = function(){
   other_files.push("自有调社会");
 
   order_files = [];
-  //order_files.push("终端销售明细表咸阳");
-  //order_files.push("终端销售明细表商洛");
+  order_files.push("终端销售明细表咸阳");
+  order_files.push("终端销售明细表商洛");
   order_files.push("终端销售明细表安康");
-  //order_files.push("终端销售明细表宝鸡");
-  //order_files.push("终端销售明细表延安");
-  //order_files.push("终端销售明细表榆林");
-  //order_files.push("终端销售明细表汉中");
+  order_files.push("终端销售明细表宝鸡");
+  order_files.push("终端销售明细表延安");
+  order_files.push("终端销售明细表榆林");
+  order_files.push("终端销售明细表汉中");
   order_files.push("终端销售明细表渭南");
   order_files.push("终端销售明细表西安");
-  //order_files.push("终端销售明细表铜川");
+  order_files.push("终端销售明细表铜川");
 
   vm.src_files_flag = order_files.concat(other_files);
   //console.log(vm.src_files_flag);
@@ -93,6 +93,7 @@ var check_order_140 = function(){
   must_field.push("归属地州");
   must_field.push("手机号码");
   must_field.push("手机串码");
+  must_field.push("销售数量");
   must_field.push("录入日期");
   
   ORDER_DETAIL.push(must_field);
@@ -104,7 +105,8 @@ var check_order_140 = function(){
 
     var obj = null;
     obj = xlsx.parse( vm.base_dir + temp_filename ); // 读入xlsx文件
-    // console.log(vm.src_files['销售订单明细']);
+    console.log(vm.order_files_flag[i]);
+    MSG.put("开始检查：" + vm.order_files_flag[i]);
     // console.log(obj);
     
     // 取出第一个sheet 的第一行。检查标题栏的内容是否正确
@@ -121,6 +123,8 @@ var check_order_140 = function(){
       order_info2 = select_col_from_array( order_info, index_must);
       ORDER_DETAIL = ORDER_DETAIL.concat( _.rest(order_info2) );
       //console.log("ORDER_DETAIL.length=" + ORDER_DETAIL.length);
+    }else{
+      break;
     }
   }
 
@@ -194,16 +198,103 @@ var calc_180 = function(){
   title.push("当日时间");
   title.push("上市时长");
   title.push("日存销比");
+  var title_length = title.length;
+  function make_line(){
+    var line = [];
+    for(var i=0; i<title_length; i++){
+      line.push("");
+    }
+    return line;
+  };
 
-  // 获得机型列表
   var all_title = ORDER_DETAIL[0];
+  var city_index = find_title_index(all_title, "归属地州");
   var prod_index = find_title_index(all_title, "机型");
-  var all_prod_type = select_one_col_from_table( ORDER_DETAIL, prod_index);
-  var all_prod_type = _.rest(all_prod_type);
+  var count_index = find_title_index(all_title, "销售数量");
 
+
+  // 获得不重复的地市列表
+  var city_list = select_one_col_from_table( ORDER_DETAIL, city_index);
+  city_list = _.rest(city_list);  // 去除第0个元素：标题行
+  city_list = _.unique(city_list);
+  city_list.sort();
+  console.log(city_list);
+  MSG.put("地市列表： [" + city_list.join() + "]");
+
+  // 获得不重复的机型列表
+  var all_prod_type = select_one_col_from_table( ORDER_DETAIL, prod_index);
+  for(i=0;i<all_prod_type.length;i++){
+    if( undefined === all_prod_type[i] || "" === all_prod_type[i] ){
+      console.log("undefined at " + i );
+      ERR_MSG.put("数据错误：发现错误机型数据 " + ORDER_DETAIL[i] );
+    }
+  }
+  //console.log(all_prod_type);
+  all_prod_type = _.rest(all_prod_type);  // 去除第0个元素：标题行
+  all_prod_type = _.unique(all_prod_type);
+  all_prod_type.sort();
   console.log(all_prod_type);
-                                      
+
+  var all_city = [];
+  all_city.push("西安城区");
+  all_city.push("西安区县");
+  all_city.push("咸阳");
+  all_city.push("宝鸡");
+  all_city.push("渭南");
+  all_city.push("铜川");
+  all_city.push("延安");
+  all_city.push("榆林");
+  all_city.push("汉中");
+  all_city.push("安康");
+  all_city.push("商洛");
   
+  // 构造空白的二维数组
+  var branch_index = find_title_index(title, "分公司");
+  var result_prod_index = find_title_index(title, "机型");
+  var sales_index = find_title_index(title, "本日出库");
+  var result_array = [];
+  result_array.push(title);
+  for(var i=0; i<all_prod_type.length; i++ ){
+    var prod = all_prod_type[i];
+    for(var j=0; j<all_city.length; j++){
+      var temp = make_line();
+
+      temp[branch_index] = all_city[j];
+      temp[result_prod_index] = all_prod_type[i];
+      temp[sales_index] = 0;
+      result_array.push(temp);
+    }
+  }
+
+  // 填充数据
+  for(var i=1;i<ORDER_DETAIL.length;i++){
+    var order = ORDER_DETAIL[i];
+    var city = order[city_index];
+    var prod = order[prod_index];
+    var count = order[count_index];
+
+    for(var j=1; j<result_array.length; j++){
+      var temp = result_array[j];
+      var city2 = temp[branch_index];
+      var prod2 = temp[result_prod_index];
+      
+      if(city===city2 && prod === prod2 ){
+        temp[sales_index] += count;
+        console.log(count);
+        break;
+      }
+    }
+  }
+                      
+  // 把计算结果存入文件。
+  var buffer = xlsx.build([{name: "铺货终端报表（分地市）", data: result_array }] );
+  fs.writeFileSync( vm.base_dir + "中间文件_第一步.xlsx", buffer);
+
+  return true;
+}
+
+
+var todo_190 = function(){
   return true;
 }
 
@@ -248,10 +339,18 @@ var check_must_title = function(target, keywords ){
     if( -1 === _.indexOf(target, kw)){
       // 显示出错提示。
       ERR_MSG.put("数据出错：。无法找到「"+ kw +"」列，请检查数据的第一行。" );
+      console.log("数据出错：。无法找到「"+ kw +"」列，请检查数据的第一行。");
+      console.log(target);
       run_flag = false;
     }
   }
   return run_flag;
+}
+
+var test = function(){
+  var temp = new Array(10);
+  console.log( temp );
+  console.log( temp.length );
 }
 
 
